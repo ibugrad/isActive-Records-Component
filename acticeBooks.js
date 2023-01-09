22ad73eefdde71be453aecaaa87239763a0c645e
I. Bugra <119770524+ibugrad@users.noreply.github.com>
2023-01-09 12:25:27 +0200

    Add files via upload

---
+++ acticeBooks.js
@@ -0,0 +1,14 @@
+import { LightningElement, wire } from 'lwc';
+import getBooks from '@salesforce/apex/BookUtility.amountOfBooks';
+export default class acticeBooks extends LightningElement {
+    @wire(getBooks)getbooklist;
+    show=false;
+    display(){
+        if(this.show==true){
+            this.show=false;
+        }
+        else if(this.show==false){
+            this.show=true;
+        }
+    }
+}
