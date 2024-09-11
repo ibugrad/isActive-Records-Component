3ff357c0a409345f5add8865e26d0c593fd11b0a
Bugra <119770524+ibugrad@users.noreply.github.com>
2024-09-11 11:27:53 +0300

    Update acticeBooks.js

--- acticeBooks.js
+++ acticeBooks.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire } from 'lwc';
+KBKL askldvas d askbf e  dnldibv e, våsd qwå sjf jd jimport { LightningElement, wire } from 'lwc';
 import getBooks from '@salesforce/apex/BookUtility.amountOfBooks';
 export default class acticeBooks extends LightningElement {
     @wire(getBooks)getbooklist;
